bdb7e3043ab73dc504cdbc8c3f53c2ebb9836137
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2022-05-29 21:53:43 +0300

    FilmCard

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { Header } from './Header';
 import { Container } from './Container';
 import SearchForm from './SearchForm';
+import FilmCard from './FilmCard';
 
 const App = () => (
   <Container>
@@ -10,4 +11,4 @@ const App = () => (
 );
 
 export default App;
-console.log(SearchForm);
+console.log(FilmCard);
